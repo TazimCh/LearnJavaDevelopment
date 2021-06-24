89831f35ad804459b22096658be535264a01b01b
TazimCh <tazimchowdhury23@gmail.com>
2021-06-24 22:06:09 +0100

    commit 1

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.util.*; 
+
+public class test{
+
+    public static void main(String[] args) {
+        
+    
+        
+        System.out.println("this is a test number two "); 
+
+        System.out.println("the sound of the mountains, that are never ending and do fiercefull"); 
+
+
+            System.out.println("never print this");
+
+
+            int t = 5; 
+
+            t = (t>9)? t+1: t*2; 
+            System.out.println(t); 
+
+    }
+
+}
